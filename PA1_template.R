6d939d8dc83d867617df9a5ef0117031d99755a1
Abhay Singh <Abhay Singh>
2016-02-13 13:46:48 -0600

    no message

--- PA1_template.R
+++ PA1_template.R
@@ -1,21 +1,17 @@
-# Reproducible Research: Peer Assessment 1
 
-## Loading and preprocessing the data
-```{r loaddata}
+## ----loaddata------------------------------------------------------------
 data <- read.csv("activity.csv")
-```
 
-## What is mean total number of steps taken per day?
-```{r}
+
+## ------------------------------------------------------------------------
 library(ggplot2)
 total.steps <- tapply(data$steps, data$date, FUN=sum, na.rm=TRUE)
 qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
 mean(total.steps, na.rm=TRUE)
 median(total.steps, na.rm=TRUE)
-```
 
-## What is the average daily activity pattern?
-```{r}
+
+## ------------------------------------------------------------------------
 library(ggplot2)
 averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
                       FUN=mean, na.rm=TRUE)
@@ -23,26 +19,20 @@ ggplot(data=averages, aes(x=interval, y=steps)) +
     geom_line() +
     xlab("5-minute interval") +
     ylab("average number of steps taken")
-```
 
-Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
-```{r}
+
+## ------------------------------------------------------------------------
 averages[which.max(averages$steps),]
-```
 
-## Imputing missing values
 
-Calculate and report the total number of missing values in the dataset (i.e. the total number of rows with NAs)
-
-```{r how_many_missing}
+## ----how_many_missing----------------------------------------------------
 missing <- is.na(data$steps)
+# How many missing
 table(missing)
-```
 
-strategy for filling in all of the missing values in the dataset : Mean value for 5 minutes interval
 
-```{r}
-# Replace NA with mean value of its 5-minute interval
+## ------------------------------------------------------------------------
+# Replace each missing value with the mean value of its 5-minute interval
 fill.value <- function(steps, interval) {
     filled <- NA
     if (!is.na(steps))
@@ -53,22 +43,16 @@ fill.value <- function(steps, interval) {
 }
 filled.data <- data
 filled.data$steps <- mapply(fill.value, filled.data$steps, filled.data$interval)
-```
-New histogram of the total number of steps taken each day &  mean/median of total number of steps.
 
-```{r}
+
+## ------------------------------------------------------------------------
 total.steps <- tapply(filled.data$steps, filled.data$date, FUN=sum)
 qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
 mean(total.steps)
 median(total.steps)
-```
 
-Mean and median values are higher after imputing missing data because 0 values has been replaced by mean value of the interval.
 
-## Are there differences in activity patterns between weekdays and weekends?
-Create a new factor variable in the dataset with two levels -- "weekday" and "weekend" indicating whether a given date is a weekday or weekend day.
-
-```{r}
+## ------------------------------------------------------------------------
 weekday.or.weekend <- function(date) {
     day <- weekdays(date)
     if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
@@ -80,11 +64,9 @@ weekday.or.weekend <- function(date) {
 }
 filled.data$date <- as.Date(filled.data$date)
 filled.data$day <- sapply(filled.data$date, FUN=weekday.or.weekend)
-```
 
-Make a panel plot containing a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all weekday days or weekend days (y-axis). The plot should look something like the following, which was creating using simulated data
-```{r}
+
+## ------------------------------------------------------------------------
 averages <- aggregate(steps ~ interval + day, data=filled.data, mean)
 ggplot(averages, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) +
     xlab("5-minute interval") + ylab("Number of steps")
-```
